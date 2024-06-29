047d93f5a5fd1f771fe28fa793792b5a75153ef2
LAlinaV <ladik151104@gmail.com>
2024-06-29 12:31:56 +0300

    add button, tg

--- lab1-project2_1_Ладик/lab1Task4/app/src/main/java/com/example/lab1task4/MainActivity.java
+++ lab1-project2_1_Ладик/lab1Task4/app/src/main/java/com/example/lab1task4/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lab1task4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but = findViewById(R.id.button);
+        TextView tg = findViewById(R.id.tg);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tg.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
