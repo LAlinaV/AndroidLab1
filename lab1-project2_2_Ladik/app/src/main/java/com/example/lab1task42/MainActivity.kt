59e0cd406eeb4f2035e51d0d1b35d9ff5ba2caa8
LAlinaV <ladik151104@gmail.com>
2024-06-29 13:04:57 +0300

    add clickListener

--- lab1-project2_2_Ladik/app/src/main/java/com/example/lab1task42/MainActivity.kt
+++ lab1-project2_2_Ladik/app/src/main/java/com/example/lab1task42/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.lab1task42
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val but = findViewById<Button>(R.id.button)
+        val tg = findViewById<TextView>(R.id.tg)
+
+        but.setOnClickListener{
+            tg.visibility = View.VISIBLE
+        }
     }
 }
